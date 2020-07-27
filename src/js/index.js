04dd1121b862a5dfad3ad06ccaa527a439dc07fa
shuyang0129 <shuyang0129@gmail.com>
2020-07-27 22:45:35 +0800

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// import '../css/index.css'
+import '../css/index.scss'
+
+import num from './test'
+console.log('Imported from test.js', num)
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: [/node_modules/],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'px2rem-loader',
+            // options here
+            options: {
+              remUni: 75,
+              remPrecision: 8,
+            },
+          },
+          // 把 postcss-loader 放在 css-loader 前面 (第一步)
+          {
+            loader: 'postcss-loader',
+            options: {
+              // 傳遞 plugins 選項並載入 autoprefixer 做使用 (第二步)
+              plugins: [autoprefixer()],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        // 把 sass-loader 放在首要處理 (第一步)
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'px2rem-loader',
+            // options here
+            options: {
+              remUni: 75,
+              remPrecision: 8,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              // 傳遞 plugins 選項並載入 autoprefixer 做使用 (第二步)
+              plugins: [autoprefixer()],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+}
